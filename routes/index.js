81e9103c207b7a7002778da24cdac4e385e85690
Karol <kpulawski13@gmail.com>
2024-02-23 16:47:15 +0000

    created messages const

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+  {
+    text: 'hi there!',
+    user: 'Arthur',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Karol',
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
