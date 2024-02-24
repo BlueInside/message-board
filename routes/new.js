a29984f1eafe7ae6b2d007f804c81b13259b0c11
Karol <kpulawski13@gmail.com>
2024-02-24 09:50:21 +0000

    moved /post to index

--- routes/new.js
+++ routes/new.js
@@ -1,9 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('hello from new');
-});
 
 module.exports = router;
