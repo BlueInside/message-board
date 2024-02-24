882884653f7a86ccec9d9941c4621685e578c53c
Karol <kpulawski13@gmail.com>
2024-02-24 09:50:09 +0000

    added post

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const messages = [
   {
-    text: 'hi there!',
-    user: 'Arthur',
+    text: "hi there!",
+    user: "Arthur",
     added: new Date(),
   },
   {
-    text: 'Hello World!',
-    user: 'Karol',
+    text: "Hello World!",
+    user: "Karol",
     added: new Date(),
   },
 ];
 
+router.post("/new", function (req, res) {
+  const messageText = req.body.comment;
+  const messageUser = req.body.name;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect("/");
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { message: "Hello" });
+});
+
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Mini Messageboard', messages: messages });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
 module.exports = router;
